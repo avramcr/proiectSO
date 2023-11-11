aac9d1eb3a80efee6bbb2a47f1f3050996712f4a
avramcr <elena.avram@student.upt.ro>
2023-11-11 12:21:45 +0200

    first commit

--- so.c
+++ so.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
@@ -7,33 +8,42 @@
 #include <time.h>
 
 typedef struct {
-    int size;
- 
+    uint16_t signature;   
+    uint32_t file_size;    
+    uint32_t reserved;
+    uint32_t data_offset; 
 } Header;
-
 typedef struct {
-    int header_size;
-    int width;
-    int height;
+    uint32_t size; // size headerinfo=40;
+    uint32_t width;
+    uint32_t height;
+    uint16_t planes;
+    uint16_t bit_count; 
+    uint32_t compression;
+    uint32_t image_size;
+    uint32_t x_pixels_per_m;
+    uint32_t y_pixels_per_m;
+    uint32_t colors_used;
+    uint32_t colors_important;
 } HeaderInfo;
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         perror("Număr incorect de argumente");
-        exit(-1);
+        exit(EXIT_FAILURE);
     }
 
     int input_destination = open(argv[1], O_RDONLY);
     if (input_destination == -1) {
         perror("Eroare deschidere fisier");
-        exit(-1);
+        exit(EXIT_FAILURE);
     }
 
     struct stat file_info;
     if (stat(argv[1], &file_info) == -1) {
         perror("Eroare la primire informatii");
         close(input_destination);
-        exit(-1);
+        exit(EXIT_FAILURE);
     }
 
     Header header;
@@ -41,7 +51,7 @@ int main(int argc, char *argv[]) {
     if (citeste == -1) {
         perror("Eroare citire fisier header");
         close(input_destination);
-        exit(-1);
+       exit(EXIT_FAILURE);
     }
 
     HeaderInfo info;
@@ -49,7 +59,7 @@ int main(int argc, char *argv[]) {
     if (citeste == -1) {
         perror("Eroare la citire fisier info");
         close(input_destination);
-        exit(-1);
+        exit(EXIT_FAILURE);
     }
 
     off_t total_size = file_info.st_size;
@@ -122,21 +132,20 @@ int main(int argc, char *argv[]) {
 
     char buffer[4096];
     sprintf(buffer, "Numele pozei este: %s\nInaltimea este: %d\nLungimea este: %d\nDimensiunea totala a fisierului este: %ld\nIdentificatorul utilizatorului: %d\nUltima modificare: %sNumarul de legaturi este: %ld\nDrepturile de acces ale owner-ului: %s\nDrepturile de acces ale grupului: %s\nDrepturile de acces altii: %s\n",
-            "poza.bmp", info.height, header.size, total_size, owner, ctime(&modificare), legaturi,user,grup,altii);
+            argv[1], info.height, info.width, total_size, owner, ctime(&modificare), legaturi,user,grup,altii);
 
     int output_destination = open("statistica.txt", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
     if (output_destination == -1) {
         perror("Eroare la deschiderea fisierului de output");
         close(input_destination);
-        exit(-1);
+        exit(EXIT_FAILURE);
     }
 
     ssize_t scrie = write(output_destination, buffer, strlen(buffer));
     if (scrie == -1) {
-        perror("Eroare la scriere in fisier");
+        perror("Eroare la scriere in fisier"); 
         close(output_destination);
-        close(input_destination);
-        exit(-1);
+         exit(EXIT_FAILURE);
     }
 
     close(output_destination);
